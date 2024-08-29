21202aa01b9b695f28e0c72cf6ad7b1105b73974
Eduardo Augusto <eduardo.augusto@ipmedia.com.br>
2024-08-28 23:03:16 -0300

    feature: manager destroy employee

--- app/Application/User/Http/Controllers/UserController.php
+++ app/Application/User/Http/Controllers/UserController.php
@@ -6,10 +6,12 @@ use App\Application\User\Http\Requests\StoreUserRequest;
 use App\Application\User\Http\Resources\UserResource;
 use App\Domain\Address\Exceptions\AddressSearchException;
 use App\Domain\User\Actions\CreateUserAction;
+use App\Domain\User\Actions\DestroyEmployeeAction;
 use App\Domain\User\Actions\EmployeeListAction;
 use App\Domain\User\DTO\UserCreateDto;
 use App\Support\Laravel\Http\Controllers\Controller;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -25,6 +27,23 @@ class UserController extends Controller
         $employees = $employeeListAction->execute();
 
         return UserResource::collection($employees);
+    }
+
+    /**
+     * Manager delete the Employee
+     *
+     * @param DestroyEmployeeAction $action
+     * @param int $employeeId
+     * @return Response
+     */
+    public function destroyEmployee(DestroyEmployeeAction $action, int $employeeId): Response
+    {
+        $action->execute(
+            manager: Auth::user(),
+            employeeId: $employeeId
+        );
+
+        return response()->noContent();
     }
 
     /**
